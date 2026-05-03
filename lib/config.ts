0ae4644a025cbc8fb59ed44d50691e9d31e5e26c
elfynity <devgirl@disroot.org>
2026-05-03 12:49:49 +0200

    changed link in config for images

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 export const SITE_NAME = "Lekhotah Clothing and Cases";
 
-export const IMG_PATH = 'https://lekhotah.devdesign.co.za/json/images';
+export const IMG_PATH = 'http://devdesign.co.za/json/images';
